4bc36156d8cf784006ed11768c1297ec9c3c5c4f
kaka <kaka@kaka-260-P020il>
2020-12-14 15:13:01 +0530

    This Keyword

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+
+class A{
+	void m1(){
+		System.out.println("m1 -A");
+	}
+	void m2() {
+		System.out.println("m2-A");
+		m1();
+		this.m1();
+	}
+}
+public class Test {
+
+	public static void main(String[] args) {
+		A a = new A();
+		a.m2();
+	}
+
+}
